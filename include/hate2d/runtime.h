5575e368a8b82575fd5114b30cde32f9541aca32
trindadedev13 <trindadedev13@gmail.com>
2025-08-06 10:24:29 -0300

    fix(Runtime): minor fix, missing delay function at header

--- include/hate2d/runtime.h
+++ include/hate2d/runtime.h
@@ -1,11 +1,10 @@
 #ifndef __HATE2D_RUNTIME_H__
 #define __HATE2D_RUNTIME_H__
 
-#include <stdbool.h>
-
 #include "hate2d/lua.h"
 
 int hate2d_lua_runtime_stop(lua_State*);
+int hate2d_lua_runtime_delay(lua_State*);
 
 void hate2d_lua_runtime_register(lua_State*);
 
